29661939a578893499c14901fa13f1eda37a5eb5
gianfoegerlim <gian@tecmaran.com.br>
2018-11-08 16:12:48 -0200

    criamos classe page

--- index.php
+++ index.php
@@ -2,17 +2,32 @@
 
 require_once("vendor/autoload.php");
 
-$app = new \Slim\Slim();
+//esse slim é namespace declarado la no arquivo autoload_namespace.php
+use \Slim\Slim;
+
+//esse caminho esta declarado no json na pata ecommerce
+use \Hcode\Page;
+
+
+$app = new Slim();
 
 $app->config('debug', true);
 
+
+//redirecionamento de rotas a / é pasta raiz
 $app->get('/', function() {
     
 	//echo "OK";
 
-	$sql = new Hcode\DB\sql();
-	$results = $sql->select("SELECT * FROM tb_users");
-    echo json_encode($results);
+	//$sql = new Hcode\DB\sql();
+	//$results = $sql->select("SELECT * FROM tb_users");
+    //echo json_encode($results);
+
+$page = new Page();
+
+$page->setTpl("index");
+
+
 });
 
 $app->run();
